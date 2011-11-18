c74ca68a28f1286e039eadc5d31cadc67452af8b
Devin Carlson <devin@devincarlson.ca>
2011-11-18 12:56:48 -0500

    By Devin Carlson: Fixed miscellaneous API problems.

--- nivo_slider.api.php
+++ nivo_slider.api.php
@@ -2,11 +2,11 @@
 /**
  * Hook to register a Nivo Slider theme.
  */
-function hook_nivo_slider_style() {
+function hook_nivo_slider_theme() {
   return array(
     'theme' => array(
-      'name' => 'style_name', // Human readable style name
-      'description' => t('Theme description.'), // Description of the style
+      'name' => t('Theme name'), // Human readable theme name
+      'description' => t('Theme description.'), // Description of the theme
       'path' => array(
         'css' => drupal_get_path('module', 'my_module') . '/css/', // The path (including a trailing slash) to the theme's stylesheet location
         'js' => drupal_get_path('module', 'my_module') . '/js/', // The path (including a trailing slash) to the theme's javascript location
