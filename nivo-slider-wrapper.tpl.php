461c66a05fcf274f7ce6a9274070cb83e839472f
Devin Carlson <devin@devincarlson.ca>
2011-11-21 12:09:00 -0500

    Issue #1348454 by Devin Carlson: Use a template for banner generation to facilitate easier theming.

---
+++ nivo-slider-wrapper.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for displaying a banner.
+ *
+ * A banner wraps slides in HTML, which provides an anchor for the Nivo Slider
+ * to target and create a slideshow with the appropriate settings and theming.
+ *
+ * Available variables:
+ * - $theme: String containing the name of the currently active theme.
+ * - $banners: String of HTML representing a banner.
+ *
+ * @see template_preprocess()
+ * @see template_process()
+ */
+?>
+<div class="slider-wrapper theme-<?php print $theme; ?>">
+  <div id="slider" class="nivoSlider">
+    <?php print $banners; ?>
+  </div>
+</div>
