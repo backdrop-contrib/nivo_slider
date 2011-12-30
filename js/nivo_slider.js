4ab3ada8f908808718fa21327884a18e99fb9292
Devin Carlson <devin@devincarlson.ca>
2011-12-30 08:55:31 -0500

    Issue #1389396 by fanton, Devin Carlson: Fixed issue with old versions of Internet Explorer by removing a trailing comma from the final Nivo Slider property.

--- js/nivo_slider.js
+++ js/nivo_slider.js
@@ -1,25 +1,25 @@
 (function ($) {
   $(document).ready(function(){
     $('#block-nivo-slider-nivo-slider #slider').nivoSlider({
-        effect: Drupal.settings.nivo_slider.effect, // Specify sets like: 'fold,fade,sliceDown'
-        slices: Drupal.settings.nivo_slider.slices, // For slice animations
-        boxCols: Drupal.settings.nivo_slider.boxCols, // For box animations
-        boxRows: Drupal.settings.nivo_slider.boxRows, // For box animations
-        animSpeed: Drupal.settings.nivo_slider.animSpeed, // Slide transition speed
-        pauseTime: Drupal.settings.nivo_slider.pauseTime, // How long each slide will show
-        startSlide: Drupal.settings.nivo_slider.startSlide, // Set starting Slide (0 index)
-        directionNav: Drupal.settings.nivo_slider.directionNav, // Next & Prev navigation
-        directionNavHide: Drupal.settings.nivo_slider.directionNavHide, // Only show on hover
-        controlNav: Drupal.settings.nivo_slider.controlNav, // 1,2,3... navigation
-        controlNavThumbs: Drupal.settings.nivo_slider.controlNavThumbs, // Use thumbnails for Control Nav
-        controlNavThumbsFromRel: true, // Use image rel for thumbs
-        keyboardNav: Drupal.settings.nivo_slider.keyboardNav, // Use left & right arrows
-        pauseOnHover: Drupal.settings.nivo_slider.pauseOnHover, // Stop animation while hovering
-        manualAdvance: Drupal.settings.nivo_slider.manualAdvance, // Force manual transitions
-        captionOpacity: Drupal.settings.nivo_slider.captionOpacity, // Universal caption opacity
-        prevText: Drupal.settings.nivo_slider.prevText, // Prev directionNav text
-        nextText: Drupal.settings.nivo_slider.nextText, // Next directionNav text
-        randomStart: Drupal.settings.nivo_slider.randomStart, // Start on a random slide
+        'effect': Drupal.settings.nivo_slider.effect, // Specify sets like: 'fold,fade,sliceDown'
+        'slices': Drupal.settings.nivo_slider.slices, // For slice animations
+        'boxCols': Drupal.settings.nivo_slider.boxCols, // For box animations
+        'boxRows': Drupal.settings.nivo_slider.boxRows, // For box animations
+        'animSpeed': Drupal.settings.nivo_slider.animSpeed, // Slide transition speed
+        'pauseTime': Drupal.settings.nivo_slider.pauseTime, // How long each slide will show
+        'startSlide': Drupal.settings.nivo_slider.startSlide, // Set starting Slide (0 index)
+        'directionNav': Drupal.settings.nivo_slider.directionNav, // Next & Prev navigation
+        'directionNavHide': Drupal.settings.nivo_slider.directionNavHide, // Only show on hover
+        'controlNav': Drupal.settings.nivo_slider.controlNav, // 1,2,3... navigation
+        'controlNavThumbs': Drupal.settings.nivo_slider.controlNavThumbs, // Use thumbnails for Control Nav
+        'controlNavThumbsFromRel': true, // Use image rel for thumbs
+        'keyboardNav': Drupal.settings.nivo_slider.keyboardNav, // Use left & right arrows
+        'pauseOnHover': Drupal.settings.nivo_slider.pauseOnHover, // Stop animation while hovering
+        'manualAdvance': Drupal.settings.nivo_slider.manualAdvance, // Force manual transitions
+        'captionOpacity': Drupal.settings.nivo_slider.captionOpacity, // Universal caption opacity
+        'prevText': Drupal.settings.nivo_slider.prevText, // Prev directionNav text
+        'nextText': Drupal.settings.nivo_slider.nextText, // Next directionNav text
+        'randomStart': Drupal.settings.nivo_slider.randomStart // Start on a random slide
     });
   });
 })(jQuery);
