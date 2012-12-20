75d921cdd8d34030c3264451d97c18a01a8e3cfb
Devin Carlson <devin@devincarlson.ca>
2012-12-20 13:20:19 -0500

    Issue #1871450 by Devin Carlson: Added an alter hook to allow slider theme information to be modified.

--- nivo_slider.api.php
+++ nivo_slider.api.php
@@ -9,17 +9,16 @@
  */
 
 /**
- * Register a custom slider theme.
+ * Register slider themes.
  *
- * This hook can be used to register a custom theme for the slider. Custom
- * themes will be displayed and made selectable on the slider options
- * administration page.
+ * This hook can be used to register themes for the slider. Themes will be
+ * displayed and made selectable on the slider options administration page.
  *
- * Custom slider themes get a unique CSS class to use for styling and can
- * specify an unlimited number of CSS and JS files to include when the slider
- * is displayed.
+ * Slider themes get a unique CSS class to use for styling and can specify an
+ * unlimited number of CSS and JS files to include when the slider is
+ * displayed.
  */
-function hook_nivo_slider_theme() {
+function hook_nivo_slider_theme_info() {
   return array(
     'theme_name' => array(
       'name' => t('Theme name'), // Human readable theme name
@@ -37,4 +36,25 @@ function hook_nivo_slider_theme() {
       ),
     )
   );
+}
+
+/**
+ * Alter slider themes.
+ *
+ * @param $themes
+ *   The associative array of theme information from
+ *   hook_nivo_slider_theme_info().
+ *
+ * @see hook_nivo_slider_theme_info()
+ */
+function hook_nivo_slider_theme_info_alter(&$themes) {
+  // Modify the default theme's name and description
+  $themes['default']['name'] = t('My theme');
+  $themes['default']['description'] = t('An excellent theme to appropriate for your own use!');
+
+  // Disable thumbnail support
+  $themes['light']['thumb_support'] = FALSE;
+
+  // Replace the default theme styling
+  $themes['dark']['resources']['css'] = drupal_get_path('module', 'my_module') . '/my_theme.css';
 }
